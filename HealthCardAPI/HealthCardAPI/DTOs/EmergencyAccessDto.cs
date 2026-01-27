80cba5d93e9ee444c7312e329a2dc1b0e2e8ab05
Kedar Mane <kedarmane3016@gmail.com>
2026-01-27 22:09:51 +0530

    Add Frontend and Backend

---
+++ HealthCardAPI/HealthCardAPI/DTOs/EmergencyAccessDto.cs
@@ -0,0 +1,8 @@
+﻿namespace HealthCardAPI.DTOs
+{
+    public class EmergencyAccessDto
+    {
+        public string HealthCardNumber { get; set; }
+        public long NomineePhone { get; set; }
+    }
+}
